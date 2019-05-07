00d6b4430b2409e06d6bce9d7156b5aa9b93ddd9
chu1070y <chu1070y@naver.com>
2019-05-07 17:46:27 +0900

    beautiful query

--- practice01.sql
+++ practice01.sql
@@ -1,102 +1,133 @@
 -- 기본 SQL 문제입니다.
 
--- 문제1.
--- 사번이 10944인 사원의 이름은(전체 이름)
--- 결과: Remko Zyda
-select concat(first_name, ' ', last_name) as '전체이름' 
-from employees 
-where emp_no = '10944';
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS '전체이름'
+FROM
+    employees
+WHERE
+    emp_no = '10944';
 
 
 -- 문제2. 
 -- 전체직원의 다음 정보를 조회하세요. 가장 선임부터 출력이 되도록 하세요. 출력은 이름, 성별,  입사일 순서이고 “이름”, “성별”, “입사일로 컬럼 이름을 대체해 보세요.
 -- 결과: sql 실행하세요
-select
-	concat(first_name, ' ', last_name) as '이름',
-    gender as '성별',
-    hire_date as '입사일'
-from 
-	employees
-order by
-	hire_date asc
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS '이름',
+    gender AS '성별',
+    hire_date AS '입사일'
+FROM
+    employees
+ORDER BY hire_date ASC
 ;
 
 
 -- 문제3.
 -- 여직원과 남직원은 각 각 몇 명이나 있나요?
 -- 결과: 남직원 수 - 179973명 / 여직원 수 - 120051명
-select
-	(select count(emp_no) from employees where gender = 'M') as '남직원 수',
-    (select count(emp_no) from employees where gender = 'F') as '여직원 수'
+SELECT 
+    (SELECT 
+            COUNT(emp_no)
+        FROM
+            employees
+        WHERE
+            gender = 'M') AS '남직원 수',
+    (SELECT 
+            COUNT(emp_no)
+        FROM
+            employees
+        WHERE
+            gender = 'F') AS '여직원 수'
 ;
 
 
 -- 문제4.
 -- 현재 근무하고 있는 직원 수는 몇 명입니까? (salaries 테이블을 사용합니다.) 
 -- 결과: 240124명
-select count(emp_no) as '현재 근무직원 수' 
-from salaries 
-where to_date > now();
+SELECT 
+    COUNT(emp_no) AS '현재 근무직원 수'
+FROM
+    salaries
+WHERE
+    to_date > NOW();
 
 
 -- 문제5.
 -- 부서는 총 몇 개가 있나요?
 -- 결과: 9개
-select count(distinct dept_no) as '부서 갯수' 
-from dept_emp;
+SELECT 
+    COUNT(DISTINCT dept_no) AS '부서 갯수'
+FROM
+    dept_emp;
 
 
 -- 문제6.
 -- 현재 부서 매니저는 몇 명이나 있나요?(역임 매너저는 제외)
 -- 결과: 9명
-select count(*) as '부서 매니저 수' 
-from dept_manager 
-where to_date > now();
+SELECT 
+    COUNT(*) AS '부서 매니저 수'
+FROM
+    dept_manager
+WHERE
+    to_date > NOW();
 
 
 -- 문제7.
 -- 전체 부서를 출력하려고 합니다. 순서는 부서이름이 긴 순서대로 출력해 보세요.
 -- 결과: sql 실행하세요
-select * 
-from departments 
-order by length(dept_name) desc;
+SELECT 
+    *
+FROM
+    departments
+ORDER BY LENGTH(dept_name) DESC;
 
 
 -- 문제8.
 -- 현재 급여가 120,000이상 받는 사원은 몇 명이나 있습니까?
 -- 결과: 2159명
-select count(emp_no) as cnt 
-from salaries 
-where salary >= 120000 
-	and to_date > now();
+SELECT 
+    COUNT(emp_no) AS cnt
+FROM
+    salaries
+WHERE
+    salary >= 120000 AND to_date > NOW();
 
 
 -- 문제9.
 -- 어떤 직책들이 있나요? 중복 없이 이름이 긴 순서대로 출력해 보세요.
 -- 결과: sql 실행하세요
-select title 
-from titles 
-group by title 
-order by length(title) desc;
+SELECT 
+    title
+FROM
+    titles
+GROUP BY title
+ORDER BY LENGTH(title) DESC;
 
 
 -- 문제10
 -- 현재 Enginner 직책의 사원은 총 몇 명입니까?
 -- 결과: 30983 명
-select
-	count(employees.emp_no) as '엔지니어 수'
-from
-	employees, 
-	(select emp_no from titles where title = 'Engineer' and to_date > now()) as engineer
-where
-	employees.emp_no = engineer.emp_no
+SELECT 
+    COUNT(employees.emp_no) AS '엔지니어 수'
+FROM
+    employees,
+    (SELECT 
+        emp_no
+    FROM
+        titles
+    WHERE
+        title = 'Engineer' AND to_date > NOW()) AS engineer
+WHERE
+    employees.emp_no = engineer.emp_no
 ;
 
 
 -- 문제11
 -- 사번이 13250(Zeydy)인 지원이 직책 변경 상황을 시간순으로 출력해보세요.
 -- 결과: sql 실행하세요
-select * 
-from titles 
-where emp_no='13250' 
-order by from_date;
+SELECT 
+    *
+FROM
+    titles
+WHERE
+    emp_no = '13250'
+ORDER BY from_date;
--- practice03.sql
+++ practice03.sql
@@ -1,112 +1,174 @@
 -- 테이블간 조인(JOIN) SQL 문제입니다.
 
--- 문제 1. 
--- 현재 급여가 많은 직원부터 직원의 사번, 이름, 그리고 연봉을 출력 하시오.
-select a.emp_no '사번', concat(a.first_name,' ',a.last_name) '이름', b.salary '연봉'
-from employees a, salaries b
-where a.emp_no = b.emp_no
-	and b.to_date = '9999-01-01'
-order by b.salary desc
+SELECT 
+    a.emp_no '사번',
+    CONCAT(a.first_name, ' ', a.last_name) '이름',
+    b.salary '연봉'
+FROM
+    employees a,
+    salaries b
+WHERE
+    a.emp_no = b.emp_no
+        AND b.to_date = '9999-01-01'
+ORDER BY b.salary DESC
 ;
 
 -- 문제2.
 -- 전체 사원의 사번, 이름, 현재 직책을 이름 순서로 출력하세요.
-select a.emp_no '사번', concat(a.first_name,' ',a.last_name) '이름', b.title '직책'
-from employees a, titles b
-where a.emp_no = b.emp_no
-	and b.to_date = '9999-01-01'
-order by concat(a.first_name,' ',a.last_name)
+SELECT 
+    a.emp_no '사번',
+    CONCAT(a.first_name, ' ', a.last_name) '이름',
+    b.title '직책'
+FROM
+    employees a,
+    titles b
+WHERE
+    a.emp_no = b.emp_no
+        AND b.to_date = '9999-01-01'
+ORDER BY CONCAT(a.first_name, ' ', a.last_name)
 ;
 
 -- 문제3.
 -- 전체 사원의 사번, 이름, 현재 부서를 이름 순서로 출력하세요..
-select a.emp_no '사번', concat(a.first_name,' ',a.last_name) '이름', c.dept_name '부서'
-from employees a, dept_emp b, departments c
-where a.emp_no = b.emp_no
-	and b.dept_no = c.dept_no
-    and b.to_date = '9999-01-01'
-order by concat(a.first_name,' ',a.last_name)
+SELECT 
+    a.emp_no '사번',
+    CONCAT(a.first_name, ' ', a.last_name) '이름',
+    c.dept_name '부서'
+FROM
+    employees a,
+    dept_emp b,
+    departments c
+WHERE
+    a.emp_no = b.emp_no
+        AND b.dept_no = c.dept_no
+        AND b.to_date = '9999-01-01'
+ORDER BY CONCAT(a.first_name, ' ', a.last_name)
 ;
 
 -- 문제4.
 -- 전체 사원의 사번, 이름, 연봉, 직책, 부서를 모두 이름 순서로 출력합니다.
-select a.emp_no '사번', concat(a.first_name,' ',a.last_name) '이름', e.salary '연봉', d.title '직책', c.dept_name '부서'
-from employees a, dept_emp b, departments c, titles d, salaries e
-where a.emp_no = b.emp_no
-	and b.dept_no = c.dept_no
-    and a.emp_no = d.emp_no
-    and a.emp_no = e.emp_no
-    and b.to_date = '9999-01-01'
-    and d.to_date = '9999-01-01'
-    and e.to_date = '9999-01-01'
-order by concat(a.first_name,' ',a.last_name)
+SELECT 
+    a.emp_no '사번',
+    CONCAT(a.first_name, ' ', a.last_name) '이름',
+    e.salary '연봉',
+    d.title '직책',
+    c.dept_name '부서'
+FROM
+    employees a,
+    dept_emp b,
+    departments c,
+    titles d,
+    salaries e
+WHERE
+    a.emp_no = b.emp_no
+        AND b.dept_no = c.dept_no
+        AND a.emp_no = d.emp_no
+        AND a.emp_no = e.emp_no
+        AND b.to_date = '9999-01-01'
+        AND d.to_date = '9999-01-01'
+        AND e.to_date = '9999-01-01'
+ORDER BY CONCAT(a.first_name, ' ', a.last_name)
 ;
 
 -- 문제5.
 -- ‘Technique Leader’의 직책으로 과거에 근무한 적이 있는 모든 사원의 사번과 이름을 출력하세요. (현재 ‘Technique Leader’의 직책(으로 근무하는 사원은 고려하지 않습니다.) 이름은 first_name과 last_name을 합쳐 출력 합니다.
-select a.emp_no '사번',  concat(a.first_name,' ',a.last_name) '이름'
-from employees a, titles b
-where a.emp_no = b.emp_no
-	and b.to_date != '9999-01-01'
+SELECT 
+    a.emp_no '사번',
+    CONCAT(a.first_name, ' ', a.last_name) '이름'
+FROM
+    employees a,
+    titles b
+WHERE
+    a.emp_no = b.emp_no
+        AND b.to_date != '9999-01-01'
 ;
 
 -- 문제6.
 -- 직원 이름(last_name) 중에서 S(대문자)로 시작하는 직원들의 이름, 부서명, 직책을 조회하세요.
-select concat(a.first_name,' ',a.last_name) '이름', c.dept_name '부서', d.title '직책'
-from employees a, dept_emp b, departments c, titles d
-where a.emp_no = b.emp_no
-	and b.dept_no = c.dept_no
-    and a.emp_no = d.emp_no
-    and b.to_date = '9999-01-01'
-    and d.to_date = '9999-01-01'
-    and a.last_name like 'S%'
+SELECT 
+    CONCAT(a.first_name, ' ', a.last_name) '이름',
+    c.dept_name '부서',
+    d.title '직책'
+FROM
+    employees a,
+    dept_emp b,
+    departments c,
+    titles d
+WHERE
+    a.emp_no = b.emp_no
+        AND b.dept_no = c.dept_no
+        AND a.emp_no = d.emp_no
+        AND b.to_date = '9999-01-01'
+        AND d.to_date = '9999-01-01'
+        AND a.last_name LIKE 'S%'
 ;
 
 -- 문제7.
 -- 현재, 직책이 Engineer인 사원 중에서 현재 급여가 40000 이상인 사원을 급여가 큰 순서대로 출력하세요.
-select c.salary '현재급여'
-from employees a, titles b, salaries c
-where a.emp_no = b.emp_no
-	and a.emp_no = c.emp_no
-    and b.to_date = '9999-01-01'
-	and c.to_date = '9999-01-01'
-    and c.salary >= 40000
-order by c.salary desc
+SELECT 
+    c.salary '현재급여'
+FROM
+    employees a,
+    titles b,
+    salaries c
+WHERE
+    a.emp_no = b.emp_no
+        AND a.emp_no = c.emp_no
+        AND b.to_date = '9999-01-01'
+        AND c.to_date = '9999-01-01'
+        AND c.salary >= 40000
+ORDER BY c.salary DESC
 ;
 
 -- 문제8.
 -- 현재 급여가 50000이 넘는 직책을 직책, 급여로 급여가 큰 순서대로 출력하시오
-select b.title '직책', c.salary '급여'
-from employees a, titles b, salaries c
-where a.emp_no = b.emp_no
-	and a.emp_no = c.emp_no
-    and b.to_date = '9999-01-01'
-    and c.to_date = '9999-01-01'
-    and c.salary > 50000
-order by c.salary desc
+SELECT 
+    b.title '직책', c.salary '급여'
+FROM
+    employees a,
+    titles b,
+    salaries c
+WHERE
+    a.emp_no = b.emp_no
+        AND a.emp_no = c.emp_no
+        AND b.to_date = '9999-01-01'
+        AND c.to_date = '9999-01-01'
+        AND c.salary > 50000
+ORDER BY c.salary DESC
 ;
 
 -- 문제9.
 -- 현재, 부서별 평균 연봉을 연봉이 큰 부서 순서대로 출력하세요.
-select c.dept_name '부서', avg(d.salary) '평균연봉'
-from employees a, dept_emp b, departments c, salaries d
-where a.emp_no = b.emp_no
-	and b.dept_no = c.dept_no
-    and a.emp_no = d.emp_no
-    and d.to_date = '9999-01-01'
-    and b.to_date = '9999-01-01'
-group by c.dept_no
-order by avg(d.salary) desc
+SELECT 
+    c.dept_name '부서', AVG(d.salary) '평균연봉'
+FROM
+    employees a,
+    dept_emp b,
+    departments c,
+    salaries d
+WHERE
+    a.emp_no = b.emp_no
+        AND b.dept_no = c.dept_no
+        AND a.emp_no = d.emp_no
+        AND d.to_date = '9999-01-01'
+        AND b.to_date = '9999-01-01'
+GROUP BY c.dept_no
+ORDER BY AVG(d.salary) DESC
 ;
 
 -- 문제10.
 -- 현재, 직책별 평균 연봉을 연봉이 큰 직책 순서대로 출력하세요.
-select c.title '직책', avg(b.salary) '평균연봉'
-from employees a, salaries b, titles c
-where a.emp_no = b.emp_no
-	and a.emp_no = c.emp_no
-	and b.to_date = '9999-01-01'
-    and c.to_date = '9999-01-01'
-group by c.title
-order by avg(b.salary) desc
+SELECT 
+    c.title '직책', AVG(b.salary) '평균연봉'
+FROM
+    employees a,
+    salaries b,
+    titles c
+WHERE
+    a.emp_no = b.emp_no
+        AND a.emp_no = c.emp_no
+        AND b.to_date = '9999-01-01'
+        AND c.to_date = '9999-01-01'
+GROUP BY c.title
+ORDER BY AVG(b.salary) DESC
 ;
